ff289bc1895c25aa437f773456d57a52bd1dc874
TaavetMandre <taavet.mandre@gmail.com>
2024-10-24 01:13:24 +0300

    suur trigo1

--- Content/Other/ruutel.gd
+++ Content/Other/ruutel.gd
@@ -1,15 +1,18 @@
 extends CharacterBody3D
-@export var speed: float = 300
+@export_range(0.1, 10.0) var nodrift: float = 1.0
 @onready var meh = $ruutel2_0
 @onready var nav : NavigationAgent3D = $NavigationAgent3D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var suun: bool = true
 
+
+enum States{TOWER, ENEMY, ATTACK, FROG}
+var current_state : States
+
+
 func _ready():
-	#var tween = get_tree().create_tween().set_loops()
-	#tween.tween_property(self, "rotation:y" , PI, 1).from_current()
-	pass
+	current_state = States.TOWER
 
 
 func _physics_process(delta):
@@ -18,26 +21,35 @@ func _physics_process(delta):
 	
 	
 	if is_on_floor():
-		#var smoov = get_tree().create_tween().set_loops()
-		#smoov.tween_property(self, "velocity:x", speed * delta, 1).from_current()
-		#smoov.tween_property(self, "velocity:z", speed * delta, 1).from_current()
-		#smoov.tween_property(self, "velocity:x", -speed * delta, 1).from_current()
-		#smoov.tween_property(self, "velocity:z", -speed * delta, 1).from_current()
-		#velocity.x = move_toward(velocity.x, 0, speed*delta/2)
-		#velocity.z = move_toward(velocity.z, 0, speed*delta/2)
-		pass
+		if velocity :
+			var T = global_transform.looking_at(self.global_transform.origin + velocity.normalized(), Vector3(0, 1, 0)) #internetist varastatud lahendus mis on sobivaks muudetud, mulle isegi suuremosalt arusaadav
+			global_transform.basis.y=lerp(global_transform.basis.y, T.basis.y, 0.2)
+			global_transform.basis.x=lerp(global_transform.basis.x, T.basis.x, 0.2)
+			global_transform.basis.z=lerp(global_transform.basis.z, T.basis.z, 0.2)
 	
+	match current_state:
+		States.TOWER: nav.set_target_position(Vector3(0,0,0))
+		States.ENEMY: pass
+		States.ATTACK: pass
+		States.FROG: pass
 	
-	match suun:
-		true: nav.set_target_position(Vector3(50, 0 , 30))
-		false: nav.set_target_position(Vector3(40, -1, -20))
-	
-	var koht = nav.get_next_path_position()
-	var trajektoor = koht - global_position
-	var norm_traj = trajektoor.normalized()
-	velocity = norm_traj * speed * delta
-	move_and_slide()
+	var trajektoor = velocity + (nav.get_next_path_position() - global_position).normalized() * delta * nodrift * nav.max_speed
+	nav.set_velocity(trajektoor)
 
 
 func test_timer():
 	suun = !suun
+
+
+func velocity_computed(safe_velocity):
+	if current_state != States.ATTACK: velocity = safe_velocity
+	else: velocity = Vector3(0,0,0)
+	move_and_slide()
+
+
+func _on_attack_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_attack_body_exited(body):
+	pass # Replace with function body.
